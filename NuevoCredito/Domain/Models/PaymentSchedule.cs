95b8a190184a7e245ef68c1fb7835132f750447e
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-03-03 16:55:14 -0500

    Primer commit.

---
+++ NuevoCredito/Domain/Models/PaymentSchedule.cs
@@ -0,0 +1,22 @@
+﻿namespace NuevoCreditoAPI.NuevoCredito.Domain.Models;
+
+public class PaymentSchedule
+{
+    public int Id { get; set; }
+    public string Currency { get; set; }
+    public string DisbursementDate { get; set; }
+    public string PaymentDay { get; set; }
+    public string Amount { get; set; }
+    public string PropertyValue { get; set; }
+    public string TEA { get; set; }
+    public string FeesPerYear { get; set; }
+    public string GracePeriod { get; set; }
+    public string PaymentPeriod { get; set; }
+    public string TotalTerm { get; set; }
+    public string DesgravamenInsuranceRate { get; set; }
+    public string PropertyInsuranceRate { get; set; }
+    public string Postage { get; set; }
+    
+    public int UserId { get; set; }
+    public User User { get; set; }
+}
